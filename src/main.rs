be73b4a100d194eb591fa5c665f89d5935b69c78
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2024-03-20 20:09:44 +0900

    beepは使えなかった

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-use beep::beep;
-use std::{thread, time::Duration};
-
-fn main() {
-    beep_play();
-}
-
-fn beep_play() {
-    beep(440);
-    thread::sleep(Duration::from_millis(500));
-    beep(880);
-    thread::sleep(Duration::from_millis(500));
-    beep(0);
-}
+fn main() {}
